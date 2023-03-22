0f499347ce9c4dd7a6116a5867ac4fc9548a91b1
Nicholas Condon <nick@nickcondon.com>
2023-03-23 07:56:14 +1100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Nicholas Condon on 23/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
